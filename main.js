ac05561cf7a1052413a86382d84183b1332fbd7d
favourA205 <favouragbejule@gmail.com>
2021-05-16 15:09:43 +0100

    More Frontend functionality added

--- main.js
+++ main.js
@@ -10,35 +10,35 @@ const sr = ScrollReveal({
 // SCROLL HOME
 
 sr.reveal('.home__title',{})
-sr.reveal('.home__scroll',{delay:200})
-sr.reveal('.home__img',{origin:'right', delay:400})
+sr.reveal('.home__subtitle',{delay:200})
+sr.reveal('.home__description',{delay:200})
+sr.reveal('.home__contact',{origin:'left', delay:400})
 
 
-// SCROLL ABOUT
+// SCROLL LOGO
 
-sr.reveal('.about__img',{delay:500})
-sr.reveal('.about__subtitle',{delay:300})
-sr.reveal('.about__profession',{delay:400})
-sr.reveal('.about__text',{delay:500})
-sr.reveal('.about__social-icon',{delay:600, interval:200})
+sr.reveal('.logo__group',{delay:500})
+// sr.reveal('.about__subtitle',{delay:300})
+// sr.reveal('.about__profession',{delay:400})
+// sr.reveal('.about__text',{delay:500})
+// sr.reveal('.about__social-icon',{delay:600, interval:200})
 
 
 
-// SCROLL SKLLLS
-sr.reveal('.skills__subtitle',{})
-sr.reveal('.skills__name',{distance:'20px', delay:50, interval:100})
-sr.reveal('.skills__img',{delay:400})
+// SCROLL SHOWCASE
+sr.reveal('.showcase__titles',{})
+sr.reveal('.row1',{distance:'20px', delay:50, interval:100})
+sr.reveal('.row2',{delay:400})
+sr.reveal('.row3',{delay:400})
 
-// SCROLL PORTFOLIO
 
-sr.reveal('.portfolio__img',{interval:200})
+// SCROLL TESTIMONIAL
+
+sr.reveal('.testimonialz',{interval:200})
 
 // SCROLL CONTACT
 
-sr.reveal('.contact__subtitle',{})
-sr.reveal('.contact__text',{interval:200})
-// sr.reveal('.contact__input',{delay:400})
+sr.reveal('.contact__title',{})
+sr.reveal('.contact__subtitle',{interval:200})
 sr.reveal('.contact__first-batch',{delay:400})
 sr.reveal('.contact__second-batch',{delay:500})
-sr.reveal('.contact__third-batch',{delay:600})
-// sr.reveal('.contact__button',{delay:600})
