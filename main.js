d8302106073d5c90d5d142d2ce84162ba12bf5c1
favourA205 <favouragbejule@gmail.com>
2021-05-18 15:15:49 +0100

    More Frontend functionality added

--- main.js
+++ main.js
@@ -12,24 +12,20 @@ const sr = ScrollReveal({
 sr.reveal('.home__title',{})
 sr.reveal('.home__subtitle',{delay:200})
 sr.reveal('.home__description',{delay:200})
-sr.reveal('.home__contact',{origin:'left', delay:400})
+sr.reveal('.home__contact',{origin:'left', delay:200})
 
 
 // SCROLL LOGO
 
-sr.reveal('.logo__group',{delay:500})
-// sr.reveal('.about__subtitle',{delay:300})
-// sr.reveal('.about__profession',{delay:400})
-// sr.reveal('.about__text',{delay:500})
-// sr.reveal('.about__social-icon',{delay:600, interval:200})
+sr.reveal('.logo__group',{ origin:'left',delay:500})
 
 
 
 // SCROLL SHOWCASE
 sr.reveal('.showcase__titles',{})
-sr.reveal('.row1',{distance:'20px', delay:50, interval:100})
-sr.reveal('.row2',{delay:400})
-sr.reveal('.row3',{delay:400})
+sr.reveal('.row1',{ origin:'left',distance:'20px', delay:50, interval:100})
+sr.reveal('.row2',{ origin:'right', delay:400})
+sr.reveal('.row3',{ origin:'left',delay:400})
 
 
 // SCROLL TESTIMONIAL
@@ -41,4 +37,4 @@ sr.reveal('.testimonialz',{interval:200})
 sr.reveal('.contact__title',{})
 sr.reveal('.contact__subtitle',{interval:200})
 sr.reveal('.contact__first-batch',{delay:400})
-sr.reveal('.contact__second-batch',{delay:500})
+sr.reveal('.contact__second-batch',{ origin:'right', delay:500})
